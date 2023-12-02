9ed2f9c44a7a33455fd309030d614bee7ec70b4b
Hugo Tárrega <hugots363@gmail.com>
2023-12-02 20:39:40 +0100

    Solving second part of the problem and adding log with the problem

--- day2/cubeConundrum.cpp
+++ day2/cubeConundrum.cpp
@@ -7,13 +7,6 @@
 #include <vector>
 using namespace std;
 
-// bool possibleSet(const string& set){
-//     for(char element : set){
-        
-//     }
-//     return true;
-// }
-
 std::tuple<int, string>  getGameNumber(const string& game){
     int firstSpacePos = 0;
     int colonPos = 0;
@@ -70,6 +63,47 @@ tuple<bool, int> isValidGame(const string& games){
 
 }
 
+int getMinimumPower(const string& games){
+    vector<string> words;
+    string word;
+    int num = 0;
+    string colour = "";
+    int highestRed = 0;
+    int highestBlue = 0; 
+    int highestGreen = 0;
+
+     for (char nchar : games) {
+        if (nchar != ' ' && nchar != ',' && nchar != ';'  && nchar != ':') {
+            word += nchar;
+        } else {
+            words.push_back(word);
+            word.clear();           
+        }
+    }
+    words.push_back(word);
+    //Now we have a splitted vector obtained from the string
+    int counter = 2;
+    for(auto iter = words.begin()+2; iter != words.end(); iter=iter+3){
+        num = std::stoi(words[counter+1]);
+        colour = words[counter+2];
+        //We need to check the higher of each one
+        if(colour == "red"){
+            if(num > highestRed){highestRed = num;}      
+        }
+        else{
+            if(colour == "blue"){
+                if(num > highestBlue){highestBlue = num;}  
+            }
+            else{
+                if(num > highestGreen){highestGreen = num;}
+            }
+        }
+
+        counter+= 3;
+    }
+    return highestBlue*highestGreen*highestRed;
+}
+
 int possibleGamesIDSum(std::string pathToInput){
     string line;
     ifstream input(pathToInput);
@@ -90,7 +124,24 @@ int possibleGamesIDSum(std::string pathToInput){
     return sum;
 }
 
+int MinimumPowerSum(std::string pathToInput){
+    string line;
+    ifstream input(pathToInput);
+    int sum = 0;
+
+    if(input.is_open()){
+        while (getline(input, line)) {
+            sum += getMinimumPower(line);
+        }
+    }
+    else{
+            throw std::ios_base::failure("File not found: " + pathToInput);
+    }
+    return sum;
+}
+
 int main(){
     std::cout << "Sum of valid IDs-> " << possibleGamesIDSum("input.txt") << std::endl;
+    std::cout << "Sum of minimum cube potences-> " << MinimumPowerSum("input.txt") << std::endl;
     return 0;
 }
