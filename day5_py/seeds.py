2a52a4e0b652c87732b23cba3729cd3fd5ed4b57
Hugo Tárrega <hugots363@gmail.com>
2023-12-30 17:30:18 +0100

    Solving first problem

--- day5_py/seeds.py
+++ day5_py/seeds.py
@@ -20,10 +20,7 @@ def getNumbers(line):
 
 def getSeeds(line): 
     numbers = getNumbers(line)
-    list = []
-    for num in sorted(numbers):
-        list.append(num)
-    return list
+    return {num: num for num in numbers}
 
 def isMap(line):
     return "map" in line
@@ -31,57 +28,41 @@ def isMap(line):
 def fillMap(line,map):
     positions = getNumbers(line)
     it = 0
-    for x in range(positions[1],positions[1]+positions[2]):
-        #map[x] = positions[1]+it
-        map[x] = positions[0]+it
-        it = it +1
+    map.append((positions[0],positions[1],positions[2]))
 
-def advanceLevel(seeds,map):
-    for key in seeds:
-        if key in map:
-            orgSeed = seeds[key]
-            del seeds[key]
-            seeds[map[key]] = orgSeed
-
-def iterateDicts(lines):
-    listOfMaps = []
-    map = {}
-
+def iterateDicts(seeds,lines):
+    map = []
+    auxSeeds = seeds
     for line in lines:
         if isMap(line):
-            map = {}
+            map.clear()
         elif line == '\n':
-            listOfMaps.append(map)
+            auxSeeds = advanceSeedData(auxSeeds,map)
         else:
             fillMap(line, map)
+    return auxSeeds
 
-    return listOfMaps
+def keyInMap(seedValue,maps):
+    for map in maps:
+        start, end = map[1], map[1] + map[2] + 1
+        if seedValue in range(start, end):
+            return map[0] + (seedValue - start)
+    return -1
 
-def findSeedLocation(seed, listOfMaps):
-    key = seed
-    #print("seed: "+str(key))
-    for map in listOfMaps:
-        if key in map:
-            key = map[key]
-    return key
+def advanceSeedData(seeds,maps):
+    newSeeds = {key: keyInMap(seeds[key], maps) if keyInMap(seeds[key], maps) != -1 else seeds[key] for key in seeds}
+    print(newSeeds)
+    return newSeeds
 
-
-def getLowestSeed(seeds,listOfMaps):
-    lowestSeed = seeds[0]
-    value = 9999999999
-    for seed in seeds:
-        auxValue = findSeedLocation(seed, listOfMaps)
-        if auxValue < value:
-            lowestSeed = seed
-            value = auxValue
-    return value
+def getLowestSeed(seeds):
+    return min(seeds.values(), default=None)
  
  
 def main():
     url = "https://adventofcode.com/2023/day/5/input"
     session_cookie = "53616c7465645f5f18facde817a82dbe29570bcb5b59eb4b14d3bc50b5b3820085ff017df59c46f31767f786c266b7431fcfb1fbe5ed22a29e980753ba4e7fb2"
  
-    file = getInput(url,session_cookie)
+    #file = getInput(url,session_cookie)
     file = "/home/hutarsan/projects/leisure/advent_code_2023/day5_py/input.txt"
     #file = "/home/hutarsan/projects/leisure/advent_code_2023/day5_py/aux.txt"
     f = open(file, 'r')
@@ -90,10 +71,11 @@ def main():
 
     #Get data
     seeds = getSeeds(lines[0])
+    print(seeds)
     lines = lines[2:]
     lines.append('\n')
-    listOfMaps = iterateDicts(lines)
-    lowestSeed = getLowestSeed(seeds,listOfMaps)
+    seeds = iterateDicts(seeds,lines)
+    lowestSeed = getLowestSeed(seeds)
     print("The lowest position in seeds is "+ str(lowestSeed ))
 
  
