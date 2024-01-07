31623a64beae004ed01fb951202eb86a4663d701
Hugo Tárrega <hugots363@gmail.com>
2024-01-07 15:50:10 +0100

    WIP Day9 (not working)

---
+++ day9_cpp/mirage.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+vector<int> createNextIteration(const vector<int> &prevIter) {
+    vector<int> iter;
+    // cout << "" << endl;
+    for (size_t i = 0; i < prevIter.size() - 1; ++i) {
+        iter.push_back( prevIter[i + 1] - prevIter[i] );
+        // cout << " " << prevIter[i + 1] - prevIter[i];
+    }
+   
+    // cout << "" << endl;
+    return iter;
+}
+
+int calculateHistoricNumber(string line) {
+    vector<int> lineOfNums;
+    std::istringstream iss(line);
+    int num;
+    int numQuantity = 0;
+
+    while (iss >> num) {
+        lineOfNums.push_back(num);
+        numQuantity++;
+    }
+
+    vector<vector<int>> historics;
+    historics.push_back(lineOfNums);
+
+    // for (int num : historics[0]) {
+    //     cout << num << " ";
+    // }
+
+    int iterations = 0;
+    //Generation the pyramidal structure that stores each iteration
+    while ( (historics[iterations][0] != 0) || (historics[iterations][1] != 0)) {
+        historics.push_back(createNextIteration(historics[iterations]));
+        iterations++;
+    }
+
+    //Now we are going all the way up storing the last calculated value
+    int lastValue = 0;
+    historics[iterations].push_back(0);
+    for(int i = iterations-1; i > 0;i--){
+        lastValue = historics[i].back() + historics[i-1].back();
+        historics[i-1].push_back(lastValue);
+    }
+
+    return lastValue;
+}
+
+int main(void) {
+    int total = 0;
+    std::ifstream file("aux.txt");
+    std::string line;
+    while (std::getline(file, line)) {
+        cout << line << endl;
+        total += calculateHistoricNumber(line);
+    }
+    file.close();
+    cout << "TOTAL:" << total << endl;
+
+    return 0;
+}
