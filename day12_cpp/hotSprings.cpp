d9e874a0d3497ce8fb3b62996c581a1e54a69f50
Hugo Tárrega <hugots363@gmail.com>
2024-02-26 15:49:37 +0100

    Solving part 1

---
+++ day12_cpp/hotSprings.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string_view>
+#include <vector>
+#include <cstdint>
+#include <span>
+#include <optional>
+ 
+/*
+Theoretical approach
+
+Terms:
+n = length of the pattern (including the additional '.' at the end)
+m = number of damaged positions
+
+The algorithm explores all possible combinations of placing dominoes while respecting the damaged positions.
+It uses memorization to avoid redundant computations of subproblems.
+
+The time complexity is O(n * m).
+*/
+
+
+struct input {
+    std::string pattern;
+    std::vector<int> damaged;
+};
+ 
+bool can_place_here(std::string_view pattern, int damaged) {
+    if (pattern[damaged] == '#') return false;
+    for (int i = 0; i < damaged; ++i) {
+        if (pattern[i] == '.') return false;
+    }
+    return true;
+}
+ 
+struct memo {
+    memo(std::string_view pattern, std::span<const int> damaged)
+        : data(pattern.length() + 1, std::vector<int>(damaged.size() + 1, -1)) {}
+    std::vector<std::vector<int>> data;
+ 
+    struct node {
+        int& value;
+        explicit operator bool() const { return value != -1; }
+        int memoize(int v) { return value = v; }
+    };
+ 
+    node get(std::string_view pattern, std::span<const int> damaged) {
+        return {data[pattern.length()][damaged.size()]};
+    }
+};
+ 
+int count(std::string_view pattern, std::span<const int> damaged, memo& m) {
+    auto k = m.get(pattern, damaged);
+    if (k) return k.value;
+ 
+    if (damaged.empty()) {
+        return k.memoize(std::find(pattern.begin(), pattern.end(), '#') == pattern.end());
+    }
+ 
+    auto cur = damaged.front();
+ 
+    if (pattern.length() <= cur) return k.memoize(0);
+ 
+    int found = 0;
+ 
+    if (can_place_here(pattern, cur)) {
+        found += count(pattern.substr(cur + 1), damaged.subspan(1), m);
+    }
+ 
+    if (pattern[0] == '#') return k.memoize(found);
+ 
+    found += count(pattern.substr(1), damaged, m);
+    return k.memoize(found);
+}
+ 
+int count(const input& in) {
+    std::string aug_pattern = in.pattern + '.';
+    memo m(aug_pattern, in.damaged);
+    return count(aug_pattern, in.damaged, m);
+}
+ 
+int solve(std::span<const input> inputs) {
+    int found = 0;
+    for (auto& in : inputs) {
+        found += count(in);
+    }
+    return found;
+}
+ 
+int main() {
+    std::ifstream file("input.txt");
+    std::string line;
+    std::vector<input> inputs;
+    while (std::getline(file, line)) {
+        auto& in = inputs.emplace_back();
+ 
+        auto space = line.find(' ');
+        in.pattern = line.substr(0, space);
+ 
+        auto rest = std::string_view(line).substr(space + 1);
+ 
+        while (true) {
+            auto comma = rest.find(',');
+            in.damaged.push_back(std::stoi(std::string(rest.substr(0, comma))));
+            if (comma == std::string_view::npos) break;
+            rest = rest.substr(comma + 1);
+        }
+    }
+ 
+    std::cout << solve(inputs) << '\n';
+ 
+    return 0;
+}
